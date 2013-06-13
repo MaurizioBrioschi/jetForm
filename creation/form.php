9ae98bca0cabadbb412c958c482db3460b7233a5
ridesoft <maurizio.brioschi@ridesoft.org>
2013-06-13 14:59:56 +0200

    path for checkbox creation

--- creation/form.php
+++ creation/form.php
@@ -7,7 +7,12 @@
     <form action="subscribe.php" method="post">
         <?php        foreach ($fields as $field)    { ?>
         <div>
-            <?php echo $field; ?><input type="text" name="<?php echo $field; ?>" value="<?php echo "<?php echo \$user[\"$field\"]; ?>"; ?>" />
+            <?php echo $field; ?><input
+                <?php if($field=='privacy1'||$field=='privacy2') { ?>
+                         name="<?php echo $field; ?>" type="checkbox" value="1" <?php echo "<?php if(\$user[\"$field\"]>0) echo \"checked\"; ?>";?> />
+                <?php }else { ?>              
+                type="text" name="<?php echo $field; ?>" value="<?php echo "<?php echo \$user[\"$field\"]; ?>"; ?>" />
+                <?php } ?>
         </div>
         <?php } ?>
         <input type="submit" value="submit" />
